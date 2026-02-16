ecb3fccd34ea51899625a654e16e58e422abd59b
devserao <seraotourjo@gmail.com>
2026-02-16 16:47:01 +0600

    added the food menu items

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,12 @@ import App from "./App";
 
 // ✅ Import global CSS
 import "./index.css";
+import StoreContextProvider from "./context/StoreContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
-      <App />
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
     </BrowserRouter>
 );
