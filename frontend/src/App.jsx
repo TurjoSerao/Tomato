ae3faaa5d029bed9991674c95a5f3410c9ef4ac8
devserao <seraotourjo@gmail.com>
2026-02-16 19:53:27 +0600

    added the home page responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
@@ -6,13 +6,16 @@ import Home from './pages/Home/Home';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Cart from './pages/Cart/Cart';
 import Footer from './components/Footer/Footer';
+import LoginPopUp from './components/LoginPopUp/LoginPopUp';
 
 
 const App = () => {
+  const [showLogin,setShowLogin] = useState(false);
   return (
     <>
+    {showLogin?<LoginPopUp setShowLogin={setShowLogin}/>:<></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
