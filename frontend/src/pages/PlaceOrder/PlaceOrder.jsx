ad6afec3019dcf9143889a7a272b627b6637fa69
devserao <seraotourjo@gmail.com>
2026-02-16 15:11:30 +0600

    first commit

---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PlaceOrder.css';
+
+const PlaceOrder = () => {
+    return (
+        <div>
+            placeholder
+        </div>
+    );
+};
+
+export default PlaceOrder;
